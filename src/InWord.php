04642c12b80a5376792cf7b5b9c7a791989a2486
Md. Himel Ali <himelali@gmail.com>
2021-01-24 14:13:53 +0600

    Initialize App

---
+++ src/InWord.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Himel\InWord;
+
+use Exception;
+
+class InWord
+{
+
+    const VERSION = '1.0';
+    private $languages = array('bn');
+    private $configs = [];
+
+    public function __construct($language = 'bn') {
+        if(!in_array($language, $this->languages))
+            throw new Exception("$language is not supported language");
+        $this->configs = $this->getConfigs($language);
+    }
+
+    private function getConfigs($language) {
+        return include "lang/$language.php";
+    }
+
+    protected  function isValid($num)
+    {
+        if(!is_numeric($num) || $num>999999999999999 || strpos($num, 'E') !== false)
+            return false;
+        return true;
+    }
+
+    public function setConfig($language) {
+        $this->configs = $this->getConfigs($language);
+    }
+
+    public function getNumber($num) {
+        if(!$this->isValid($num))
+            return false;
+        return strtr($num,$this->configs['numbers']);
+    }
+
+}
