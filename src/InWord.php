cbbb1f09c9f1caa82445bde28bea3cdf9354e7f9
Md. Himel Ali <himelali@gmail.com>
2021-01-26 09:38:05 +0600

    Initialize

--- src/InWord.php
+++ src/InWord.php
@@ -4,12 +4,13 @@ namespace Himel\Web;
 
 use Exception;
 
-class InWord
-{
+class InWord {
 
     const VERSION = '1.0.0';
-    private $languages = array('bn');
-    private $configs = [];
+    protected $languages = array('bn');
+    protected $configs = [];
+    protected $number = null;
+    protected $date = null;
 
     public function __construct($language = 'bn') {
         if(!in_array($language, $this->languages))
@@ -17,7 +18,7 @@ class InWord
         $this->configs = $this->getConfigs($language);
     }
 
-    private function getConfigs($language) {
+    protected function getConfigs($language) {
         return include "lang/$language.php";
     }
 
@@ -32,10 +33,72 @@ class InWord
         $this->configs = $this->getConfigs($language);
     }
 
-    public function getNumber($num) {
+    public function setNumber($num) {
         if(!$this->isValid($num))
-            return false;
-        return strtr($num,$this->configs['numbers']);
+            throw new Exception("$num is not supported");
+        $this->number = $num;
+    }
+
+    public function getNumber() {
+        return strtr($this->number,$this->configs['numbers']);
+    }
+
+    protected function makeWord($number) {
+        if($number == 0) { return 'শূন্য'; }
+        if(is_float($number)){
+            $numbers = explode(".", $number);
+            $word = $this->numberToWord($numbers[0]);
+            if(isset($decimal[1])){
+                $word .= ' দশমিক '.$this->numberToDecimalWord((string)$decimal[1]);
+            } return $word;
+        } return $this->numberToWord($number);
+    }
+
+    public function getWord() {
+        return $this->makeWord($this->number);
+    }
+
+    protected function numberToDecimalWord($num) {
+        $decimals = str_split($num);
+        $word = '';
+        foreach ($decimals as $key => $decimal) {
+            $word .= $this->configs['words'][$decimal].' ';
+        } return rtrim($word);
+    }
+
+    protected function numberToWord($num) {
+        $word = '';
+        $million_10 = (int) ($num / 10000000);
+        if($million_10 != 0){
+            if($million_10 > 99) {
+                $word .= $this->makeWord($million_10).' কোটি ';
+            } else{
+                $word .= $this->configs['words'][$million_10].' কোটি ';
+            }
+        }
+
+        $million_10_div = $num % 10000000;
+        $lac = (int) ($million_10_div/100000);
+        if($lac > 0){
+            $word .= $this->configs['words'][$lac].' লক্ষ ';
+        }
+
+        $lac_div = $million_10_div%100000;
+        $thousand = (int) ($lac_div/1000);
+        if($thousand > 0){
+            $word .= $this->configs['words'][$thousand].' হাজার ';
+        }
+
+        $thousand_div = $lac_div%1000;
+        $hundred = (int) ($thousand_div/100);
+        if($hundred > 0){
+            $word .= $this->configs['words'][$hundred].' শত ';
+        }
+
+        $hundred_div = (int) ($thousand_div%100);
+        if($hundred_div > 0){
+            $word .= $this->configs['words'][$hundred_div];
+        } return rtrim($word);
     }
 
 }
